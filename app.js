9092df2e3d6d7b5cfeec2398485635c0480d04e0
Yuliya Shostko <shostko.jn@gmail.com>
2020-09-02 02:34:28 +0300

    slick slider added and tablet version

--- app.js
+++ app.js
@@ -6,3 +6,32 @@ $('.navigation-left > div, .navigation-right div, button').click(() => {
   $('.navigation-container').removeClass('navigation-active');
 });
 
+$(window).resize(() => {
+  const width = $(document).width();
+
+  if (width < 768) {
+    $('.title-section').slick('unslick');
+  } else {
+    $('.title-section').not('.slick-initialized').slick({
+      dots: true,
+      arrows: false,
+    });
+  }
+});
+
+/*$(document).ready(() => {
+  $('.title-section').slick({
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: "unslick"
+      },
+      {
+        breakpoint: 1400,
+
+      }
+    ],
+    dots: true,
+    arrows: false,
+  })
+})*/
